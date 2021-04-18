87c8e17486fce91669e790983d6c02abe2504ea2
kwinahn <77373020+kwinahn@users.noreply.github.com>
2021-04-18 14:16:02 -0500

    added generatemarkdown function

--- index.js
+++ index.js
@@ -1,13 +1,98 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+function promptUser() {
+	return inquirer.prompt([
+		{
+			type: 'input',
+			name: 'projectName',
+			message: 'What is the title of your project? (Required)',
+			validate: (projectInput) => {
+				if (projectInput) {
+					return true;
+				} else {
+					console.log('Please enter the title of your project.');
+					return false;
+				}
+			},
+		},
+		{
+			type: 'input',
+			name: 'description',
+			message: 'Enter a description for this project. (Required)',
+			validate: (descriptionInput) => {
+				if (descriptionInput) {
+					return true;
+				} else {
+					console.log('Please enter a short description for this project.');
+					return false;
+				}
+			},
+		},
+		{
+			type: 'input',
+			name: 'installation',
+			message: 'Any installation instructions?',
+		},
+		{
+			type: 'input',
+			name: 'projectUse',
+			message: 'What is the use of this project?',
+		},
+		{
+			type: 'input',
+			name: 'contributions',
+			message: 'How do you contribute to this project?',
+		},
+		{
+			type: 'input',
+			name: 'test',
+			message: 'Provide test instructions if there are any.',
+		},
+		{
+			type: 'list',
+			name: 'license',
+			message: 'Select a license for this project',
+			choices: ['Apache', 'CreativeCommons', 'MIT', 'Mozilla'],
+		},
+		{
+			type: 'input',
+			name: 'githubUsername',
+			message: 'What is your Github username? (Required)',
+			validate: (userInput) => {
+				if (userInput) {
+					return true;
+				} else {
+					console.log('Please enter your Github username.');
+					return false;
+				}
+			},
+		},
+		{
+			type: 'input',
+			name: 'email',
+			message: 'What is your email? (Required)',
+			validate: (emailInput) => {
+				if (emailInput) {
+					return true;
+				} else {
+					console.log('Please enter your email.');
+					return false;
+				}
+			},
+		},
+	]);
+}
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+promptUser()
+	.then((answers) => generateMarkdown(answers))
+	.then((readme) => {
+		fs.writeFile('README.md', readme, (err) => {
+			if (err) {
+				console.log(err);
+				return;
+			}
+		});
+	});
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,38 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function generateMarkdown(answers) {
+  const { projectName, description, installation, projectUse, contributions, test, license, githubUsername, email } = answers
+  return `
+![Github License](https://img.shields.io/badge/license-${license}-blue)
+# ${projectName} 
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+#### Table of Contents
+1. [Project Description](#project-description)
+2. [Installation](#installation)
+3. [Usage](#usage)
+4. [Contribution Guide](#contributions)
+5. [License](#license)
+6. [Questions](#questions)
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+## Project Description
+* ${description}
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+## Installation
+* ${installation}
 
+## Usage Information
+* ${projectUse}
+
+## Contributions
+* ${contributions}
+
+## Test Instructions
+* ${test}
+
+## License
+* Licensed under ![Github License](https://img.shields.io/badge/license-${license}-blue)
+
+## Questions
+* Find me on Github: [${githubUsername}](http://github.com/${githubUsername})
+* For questions about this project please contact me at: ${email}.
 `;
 }
 
